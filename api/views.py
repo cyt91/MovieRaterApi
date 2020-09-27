5326507f1da867be268b5a9e6f9fe1634f7ffdcf
yewtuck <cheah808@gmail.com>
2020-09-27 17:36:36 +0800

    1. create default permission classes in settings.py 2. implement permission_classes IsAuthenticated in modelViewSets 3. override builtin update and create methods in RatingViewSet to disallow post methods in ratings endpoint.

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 # Create your views here.
@@ -17,6 +18,7 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, ) # or AllowAny
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -46,4 +48,13 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    authentication_classes = (TokenAuthentication, )
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'You can\'t update ratings'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {'message': 'You can\'t create ratings'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
