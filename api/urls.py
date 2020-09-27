8fdb63949fac0b07b82d6a5de5e3a99d974b8874
yewtuck <cheah808@gmail.com>
2020-09-27 15:24:11 +0800

    Added movie and rating serializers;added movie and rating viewsets to query movie and rating objects; register viewsets in urls.py

--- api/urls.py
+++ api/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
