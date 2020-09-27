91b58f9de6a221c5fcd7ebfb15ffe46b58ed3cfa
yewtuck <cheah808@gmail.com>
2020-09-27 16:36:27 +0800

    define custom funcs avg_rating and no_of_ratings in models; return custom funcs avg_rating and no_of_ratings as fields in serializer

--- api/models.py
+++ api/models.py
@@ -8,6 +8,21 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum_of_ratings = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum_of_ratings += rating.stars
+
+        if len(ratings) > 0:
+            return sum_of_ratings/len(ratings)
+        else:
+            return 0
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
