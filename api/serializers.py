ecff0e2af9719c7b34ead861e0c625ded748e00b
yewtuck <cheah808@gmail.com>
2020-09-27 17:09:59 +0800

    1. create UserViewSet to allow post/get (register/get) users 2. register UserViewSet in urls.py as new users path 3. create UserSerializer to hide password in GET;require password in POST create new user; override built in create method

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Movie, Rating
+from django.contrib.auth.models import User
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -12,3 +13,14 @@ class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = ('id', 'stars', 'user', 'movie')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
